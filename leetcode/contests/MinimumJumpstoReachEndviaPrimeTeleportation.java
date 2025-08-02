10e6e39da39bf169ace33d301e7ac85ea6919272
someear9h <tikotkarsamarth@gmail.com>
2025-08-03 01:30:06 +0530

    more biweekly contest problems

--- leetcode/contests/MinimumJumpstoReachEndviaPrimeTeleportation.java
+++ leetcode/contests/MinimumJumpstoReachEndviaPrimeTeleportation.java
@@ -1,83 +1,131 @@
 import java.util.*;
 
 public class MinimumJumpstoReachEndviaPrimeTeleportation {
-
-    public static int minJumps(int[] nums) {
-        int MAX = 1000000;
-        int[] spf = buildSPF(MAX); // Smallest Prime Factor sieve
-
+    public int minJumps(int[] nums) {
         int n = nums.length;
-        Map<Integer, List<Integer>> primeMap = new HashMap<>();
-
-        // Step 1: Build map of prime factors to indices
-        for (int i = 0; i < n; i++) {
-            int val = nums[i];
-            Set<Integer> primes = getPrimeFactors(val, spf); // Unique primes
-            for (int p : primes) {
-                primeMap.computeIfAbsent(p, k -> new ArrayList<>()).add(i);
-            }
+        if (n == 1) 
+            return 0;
+        
+        int maxVal = 0;
+        for (int num : nums) {
+            if (num > maxVal) 
+                maxVal = num;
         }
-
-        boolean[] visited = new boolean[n];
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0, 0});
-        visited[0] = true;
-
-        while (!q.isEmpty()) {
-            int[] curr = q.poll();
-            int idx = curr[0], steps = curr[1];
-
-            if (idx == n - 1) return steps;
-
-            // Jump to i - 1 and i + 1
-            if (idx + 1 < n && !visited[idx + 1]) {
-                visited[idx + 1] = true;
-                q.add(new int[]{idx + 1, steps + 1});
-            }
-            if (idx - 1 >= 0 && !visited[idx - 1]) {
-                visited[idx - 1] = true;
-                q.add(new int[]{idx - 1, steps + 1});
-            }
-
-            // Jump to all indices sharing prime factors
-            Set<Integer> factors = getPrimeFactors(nums[idx], spf);
-            for (int prime : factors) {
-                if (primeMap.containsKey(prime)) {
-                    for (int nextIdx : primeMap.get(prime)) {
-                        if (!visited[nextIdx]) {
-                            visited[nextIdx] = true;
-                            q.add(new int[]{nextIdx, steps + 1});
-                        }
-                    }
-                    // Remove after visiting to avoid future checks
-                    primeMap.remove(prime);
-                }
-            }
-        }
-        return -1;
+        
+        int[] spf = buildSPF(maxVal);
+        boolean[] isPrime = identifyPrimes(maxVal, spf);
+        Set<Integer> appearingPrimes = getAppearingPrimes(nums, isPrime);
+        Map<Integer, List<Integer>> primeToIndices = buildPrimeToIndicesMap(nums, appearingPrimes, spf);
+        
+        return bfs(nums, primeToIndices, isPrime);
     }
 
-    // Function 1: Build Smallest Prime Factor sieve
-    private static int[] buildSPF(int max) {
+    private int[] buildSPF(int max) {
         int[] spf = new int[max + 1];
         for (int i = 2; i <= max; i++) {
             if (spf[i] == 0) {
                 for (int j = i; j <= max; j += i) {
-                    if (spf[j] == 0) spf[j] = i;
+                    if (spf[j] == 0) {
+                        spf[j] = i;
+                    }
                 }
             }
         }
         return spf;
     }
 
-    // Function 2: Prime Factorization using SPF
-    private static Set<Integer> getPrimeFactors(int x, int[] spf) {
-        Set<Integer> result = new HashSet<>();
-        while (x > 1) {
-            int p = spf[x];
-            result.add(p);
-            while (x % p == 0) x /= p;
+    private boolean[] identifyPrimes(int maxVal, int[] spf) {
+        boolean[] isPrime = new boolean[maxVal + 1];
+        for (int i = 2; i <= maxVal; i++) {
+            isPrime[i] = (spf[i] == i);
         }
-        return result;
+        return isPrime;
+    }
+
+    private Set<Integer> getAppearingPrimes(int[] nums, boolean[] isPrime) {
+        Set<Integer> appearingPrimes = new HashSet<>();
+        for (int num : nums) {
+            if (num >= 2 && isPrime[num]) {
+                appearingPrimes.add(num);
+            }
+        }
+        return appearingPrimes;
+    }
+
+    private Map<Integer, List<Integer>> buildPrimeToIndicesMap(int[] nums, Set<Integer> appearingPrimes, int[] spf) {
+        Map<Integer, List<Integer>> primeToIndices = new HashMap<>();
+        for (int p : appearingPrimes) {
+            primeToIndices.put(p, new ArrayList<>());
+        }
+        
+        for (int j = 0; j < nums.length; j++) {
+            int num = nums[j];
+            if (num == 1) 
+                continue;
+            
+            Set<Integer> factors = getPrimeFactors(num, spf);
+            for (int p : factors) {
+                if (appearingPrimes.contains(p)) {
+                    primeToIndices.get(p).add(j);
+                }
+            }
+        }
+        return primeToIndices;
+    }
+
+    private Set<Integer> getPrimeFactors(int val, int[] spf) {
+        Set<Integer> factors = new HashSet<>();
+        while (val > 1) {
+            int prime = spf[val];
+            factors.add(prime);
+            while (val % prime == 0) {
+                val /= prime;
+            }
+        }
+        return factors;
+    }
+
+    private int bfs(int[] nums, Map<Integer, List<Integer>> primeToIndices, boolean[] isPrime) {
+        int n = nums.length;
+        boolean[] visited = new boolean[n];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0, 0});
+        visited[0] = true;
+        
+        while (!queue.isEmpty()) {
+            int[] node = queue.poll();
+            int idx = node[0];
+            int steps = node[1];
+            
+            if (idx == n - 1) {
+                return steps;
+            }
+            
+            if (idx + 1 < n && !visited[idx + 1]) {
+                visited[idx + 1] = true;
+                queue.offer(new int[]{idx + 1, steps + 1});
+            }
+            
+            if (idx - 1 >= 0 && !visited[idx - 1]) {
+                visited[idx - 1] = true;
+                queue.offer(new int[]{idx - 1, steps + 1});
+            }
+            
+            int numHere = nums[idx];
+            if (numHere >= 2 && isPrime[numHere] && primeToIndices.containsKey(numHere)) {
+                List<Integer> indices = primeToIndices.get(numHere);
+                for (int j : indices) {
+                    if (j == idx) 
+                        continue;
+                    if (!visited[j]) {
+                        visited[j] = true;
+                        queue.offer(new int[]{j, steps + 1});
+                    }
+                }
+                primeToIndices.remove(numHere);
+            }
+        }
+        
+        return -1;
     }
 }
