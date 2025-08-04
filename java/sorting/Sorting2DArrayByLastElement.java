b082939c6594ce03b87d501fd308cd2a9e366466
someear9h <tikotkarsamarth@gmail.com>
2025-08-05 00:43:20 +0530

    dp 2D problems

---
+++ java/sorting/Sorting2DArrayByLastElement.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Sorting2DArrayByLastElement {
+    public static void main(String[] args) {
+        int[][] array = {{3, 8}, {5, 1}, {9, 4}};
+
+        System.out.println("original array: ");
+        System.out.println(Arrays.deepToString(array));
+
+        // Arrays.sort(array, (a, b) -> Integer.compare(a[1], b[1]));
+        Arrays.sort(array, (a, b) -> a[1] - b[1]);
+
+        System.out.println("Sorting by 2nd element: ");
+        System.out.println(Arrays.deepToString(array));
+
+        System.out.println(1 % 3);
+    }
+}
